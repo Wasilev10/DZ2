6744ac859856166ec9c05b8053ac70c5e1f26838
Wasilev10 <danilka.8h@gmail.com>
2024-12-01 16:27:40 +0300

    Add files via upload

---
+++ Переменные(new).py
@@ -0,0 +1,9 @@
+first_name = 'Python'
+
+apple_count = 1.5
+
+second_name = 12
+
+time_for_one_task = 1.5 / 12
+
+print('Курс:',first_name,',' , 'всего задач:', second_name,',' , 'затрачено часов:', apple_count,',' , 'среднее время выполнения', time_for_one_task, 'часа')
